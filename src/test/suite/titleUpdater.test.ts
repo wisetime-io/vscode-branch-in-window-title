963191407a75448be9a3daa472ec049a8898b768
Andrey Repin <anrdaemon@yandex.ru>
2023-03-10 12:15:22 +0300

    Improved tests

--- src/test/suite/titleUpdater.test.ts
+++ src/test/suite/titleUpdater.test.ts
@@ -17,6 +17,18 @@ suite('Title Updater Test Suite', () => {
     updateTitle(getTitle, setTitle, branchTemplate, branchNameIsPrefix)(branchName).then(done);
   });
 
+  test('updateTitle should prepend branch information to title', done => {
+    const branchName = 'master';
+    const branchTemplate = `[Branch: \$branchName]\${separator}`;
+    const branchNameIsPrefix = true;
+    const getTitle = (): string => 'wisetime.ts';
+    const setTitle = (title: string): Thenable<void> => {
+      assert(title === `[Branch: ${branchName}]\${separator}${getTitle()}`, 'Branch information is prepended to the title');
+      return Promise.resolve();
+    };
+    updateTitle(getTitle, setTitle, branchTemplate, branchNameIsPrefix)(branchName).then(done);
+  });
+
   test('updateTitle should remove branch information from title if branch is undefined', done => {
     const titleWithoutBranch = 'wisetime.ts';
     const branchTemplate = `\${separator}[Branch: \$branchName]`;
@@ -39,5 +51,17 @@ suite('Title Updater Test Suite', () => {
       return Promise.resolve();
     };
     updateTitle(getTitle, setTitle, branchTemplate, branchNameIsPrefix)(branchName).then(done);
+  });
+
+  test('updateTitle should not alter the title if template does not match', done => {
+    const branchName = 'master';
+    const branchTemplate = `\${separator}[Branch: \$branchName]`;
+    const branchNameIsPrefix = false;
+    const getTitle = (): string => `[master] wisetime.ts`;
+    const setTitle = (title: string): Thenable<void> => {
+      assert(false, 'setTitle should not be called');
+      return Promise.resolve();
+    };
+    updateTitle(getTitle, setTitle, branchTemplate, branchNameIsPrefix)(undefined).then(done);
   });
 });
