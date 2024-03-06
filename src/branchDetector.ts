922e2ea04bfedae0a0921d77c7af379fa0e8f04f
lsh.wuyi <lishuaihang.lsh@alibaba-inc.com>
2024-03-06 15:08:51 +0800

    support worktree and sub dir

--- src/branchDetector.ts
+++ src/branchDetector.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2020 WiseTime. All rights reserved.
 
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { execSync } from 'child_process';
 
 class BranchDetector implements vscode.Disposable {
 
@@ -8,10 +10,10 @@ class BranchDetector implements vscode.Disposable {
   private branchName: string | undefined;
 
   constructor(projectRootPath: string, pollingInterval: number, branchDidChange: (branchName: string | undefined) => void) {
-    const headFile = projectRootPath + '/.git/HEAD';
-    this.readBranchName(headFile).then(branchDidChange);
+    const gitPath = getGitPath(projectRootPath);
+    this.readBranchName(gitPath).then(branchDidChange);
     this.intervalId = setInterval(
-      () => this.readBranchName(headFile).then(b => this.callIfChanged(b, branchDidChange)),
+      () => this.readBranchName(gitPath).then(b => this.callIfChanged(b, branchDidChange)),
       pollingInterval
     );
   }
@@ -20,9 +22,28 @@ class BranchDetector implements vscode.Disposable {
     clearInterval(this.intervalId);
   }
 
-  private async readBranchName(headFilePath: string): Promise<string | undefined> {
+  private async readBranchName(gitPath: string): Promise<string | undefined> {
     try {
-      const data = await vscode.workspace.fs.readFile(vscode.Uri.file(headFilePath));
+      const gitHeadPathUri = vscode.Uri.file(gitPath);
+      let gitDir = gitPath;
+      // Check if .git is a file (worktree) or a directory (normal repository)
+      const gitFileStat = await vscode.workspace.fs.stat(gitHeadPathUri);
+      if (gitFileStat.type === vscode.FileType.File) {
+        // If .git is a file, read the file to find the path to the git directory
+        const gitFileData = await vscode.workspace.fs.readFile(gitHeadPathUri);
+        const gitFileContents = Buffer.from(gitFileData).toString('utf8').trim();
+        const match = gitFileContents.match(/gitdir:\s+(.+)$/);
+        if (match && match[1]) {
+          // Resolve the found path against the working tree root
+          gitDir = path.resolve(path.dirname(gitPath), match[1].trim());
+        } else {
+          return undefined;
+        }
+      }
+
+      // Adjust the path to point to the common GIT_DIR
+      const headFileUri = vscode.Uri.file(path.join(gitDir, 'HEAD'));
+      const data = await vscode.workspace.fs.readFile(headFileUri);
       const content = Buffer.from(data).toString('utf8');
       if (content.startsWith('ref: refs/heads/')) {
         return content.replace(/^(ref: refs\/heads\/\.*)/, '').trim();
@@ -55,4 +76,15 @@ export default function detectBranch(
   branchDidChange: (branchName: string | undefined) => void
 ): vscode.Disposable {
   return new BranchDetector(projectRootPath, pollingInterval, branchDidChange);
+}
+
+function getGitPath(projectRoot: string) {
+  let gitRoot = projectRoot;
+  try {
+    gitRoot = execSync('git rev-parse --show-toplevel', {
+      cwd: projectRoot,
+    }).toString().trim();
+  } catch (_) {
+  }
+  return path.join(gitRoot, '.git');
 }
--- src/test/suite/branchDetector.test.ts
+++ src/test/suite/branchDetector.test.ts
@@ -3,56 +3,54 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as os from 'os';
 import * as path from 'path';
 import detectBranch from '../../branchDetector';
 import { TextEncoder } from 'text-encoding';
 
 suite('Branch Detector Integration Test Suite', () => {
+  const tmpProjectRoot = path.join(os.tmpdir(), "vscode-git-branch-detector.branchDetector.test");
+  const gitRoot = path.join(tmpProjectRoot, ".git");
 
   test("branchDidChange should be called with undefined branch if the .git/HEAD file could not be read", done => {
-    deleteGitHeadFile();
-    detectBranch(resourcesPath, 100, branchName => {
+    detectBranch(tmpProjectRoot, 100, branchName => {
       assert(branchName === undefined, 'No branch detected');
       done();
     });
   });
 
   test("branchDidChange should be called with undefined branch if the .git/HEAD file does contain a branch", done => {
-    writeGitHeadFile('3d4986953423141f971dc69a691dbd89756cccbc');  // Some commit hash.
-    detectBranch(resourcesPath, 100, branchName => {
+    writeGitHeadFile(gitRoot, '3d4986953423141f971dc69a691dbd89756cccbc');  // Some commit hash.
+    detectBranch(tmpProjectRoot, 100, branchName => {
       assert(branchName === undefined, 'No branch detected');
       done();
     });
   });
 
   test("branchDidChange should be called with the branch name if the .git/HEAD file contains a branch", done => {
-    writeGitHeadFile('ref: refs/heads/master');
-    detectBranch(resourcesPath, 100, branchName => {
+    writeGitHeadFile(gitRoot, 'ref: refs/heads/master');
+    detectBranch(tmpProjectRoot, 100, branchName => {
       assert(branchName === 'master', 'A branch is detected');
       done();
     });
   });
 
-  deleteGitHeadFile();
+  fs.rmdirSync(tmpProjectRoot, { recursive: true });
 });
 
-const resourcesPath = `${__dirname}/../../../src/test/resources`;
-const gitHeadFilePath = `${resourcesPath}/.git/HEAD`;
-
-function deleteGitHeadFile(): void {
+function deleteGitHeadFile(gitRoot: string): void {
   try {
-    fs.unlinkSync(gitHeadFilePath);
-  } catch(_) {
+    fs.unlinkSync(path.join(gitRoot, 'HEAD'));
+  } catch (_) {
     // Maybe the file doesn't exist.
   }
 };
 
-function writeGitHeadFile(content: string): void {
-  const dir = path.dirname(gitHeadFilePath);
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+function writeGitHeadFile(gitDir: string, content: string): void {
+  if (!fs.existsSync(gitDir)) {
+    fs.mkdirSync(gitDir, { recursive: true });
   }
-  const uri = vscode.Uri.file(gitHeadFilePath);
+  const gitHeadFilePath = path.join(gitDir, 'HEAD');
   const data = new TextEncoder().encode(content);
   fs.writeFileSync(gitHeadFilePath, data);
 };
